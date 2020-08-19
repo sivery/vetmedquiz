56a501eb7a190ec6fa31c6df313bf1283d851fb5
Xavier Reuter <siverysilver@gmail.com>
2020-08-19 11:06:13 +0200

    update news

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -85,7 +85,7 @@ export class FormComponent implements OnInit {
 
   private showMessage() {
     this.msgs.push({severity:'info', summary:'Nouveau (15/08)', detail:'Ajout du TOSS de Septembre 2019 (128 questions)'});
-    this.msgs.push({severity:'info', summary:'Nouveau (17/08)', detail:"Ajout de l'EXMD de Juillet 2019 (parties Raisonnement et Communication) (20 questions)"});
+    this.msgs.push({severity:'info', summary:'Nouveau (17/08 & 19/08)', detail:"Ajout de l'EXMD de Juillet 2019 (parties Raisonnement, Communication, Ethique et Empathie) (42 questions)"});
   }
 
   public selectQuestionSets() {
